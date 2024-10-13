7d28a874429479f99e48b290888d3c031f7b3b65
q4niel <q4niel@protonmail.com>
2024-10-13 20:19:43 +0200

    -
    - Tokenizing function implementations moved to separate file (etcl_tokenize.cpp)
    
    - etcl.cpp renamed to etcl_load.cpp

--- dev/build.py
+++ dev/build.py
@@ -17,9 +17,10 @@ def build() -> str:
     tempDir:str = path.relative.make("dev/temp")
     proj:str = path.projectDir
 
-    cmd(f"g++ -c src/etcl.cpp -o {tempDir}/etcl.o -std=c++23")
     cmd(f"g++ -c src/etcl_getters.cpp -o {tempDir}/etcl_getters.o -std=c++23")
-    cmd(f"ar rvs {outDir}/etcl.lib {tempDir}/etcl.o {tempDir}/etcl_getters.o")
+    cmd(f"g++ -c src/etcl_load.cpp -o {tempDir}/etcl_load.o -std=c++23")
+    cmd(f"g++ -c src/etcl_tokenize.cpp -o {tempDir}/etcl_tokenize.o -std=c++23")
+    cmd(f"ar rvs {outDir}/etcl.lib {tempDir}/etcl_getters.o {tempDir}/etcl_load.o {tempDir}/etcl_tokenize.o")
 
     path.remove(tempDir)
     return outDir
---
+++ src/etcl_tokenize.cpp
@@ -0,0 +1,63 @@
+#include "etcl.hpp"
+
+bool etcl::Object::tokenize(std::string_view data, int &index, std::string stringedType, Var &var, bool(*hasValue)(std::string_view data, int &index, Var &var)) {
+    return tokenizeType(data, index, stringedType) && tokenizeKey(data, index, var.Key) && tokenizeValue(data, index, var, hasValue);
+}
+
+bool etcl::Object::tokenizeType(std::string_view data, int &index, std::string &type) {
+    switch (data[index-1]) {
+        default: return false;
+        case ' ':
+        case '{':
+        case '}': break;
+    }
+
+    type += ' ';
+
+    for (char c : type) {
+        if (c != data[index++]) return false;
+    }
+
+    return true;
+}
+
+bool etcl::Object::tokenizeKey(std::string_view data, int &index, std::string &key) {
+    do {
+        if (data[index] == ' ') {
+            if (!key.empty()) return true;
+            continue;
+        }
+
+        if (data[index] == '=' && !key.empty()) {
+            index--;
+            return true;
+        }
+
+        key += data[index];
+    } while (index++ < data.length());
+    return false;
+}
+
+bool etcl::Object::tokenizeValue(std::string_view data, int &index, Var &var, bool(*hasValue)(std::string_view data, int &index, Var &var)) {
+    bool hasEqual = false;
+
+    while (index++ < data.length()) {
+        switch (data[index]) {
+            case ' ': continue;
+
+            case '=': {
+                if (hasEqual) return false;
+                hasEqual = true;
+                break;
+            }
+
+            default: {
+                if (!hasEqual) return false;
+                goto breakWhile;
+            }
+        }
+    }
+    breakWhile:
+
+    return hasValue(data, index, var);
+}
